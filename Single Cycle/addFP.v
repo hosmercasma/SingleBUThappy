236a461cb3d07c959968fe22dcdb07e32e4af390
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2024-07-13 22:08:26 -0500

    add FP op by Josep

--- Single Cycle/addFP.v
+++ Single Cycle/addFP.v
@@ -3,37 +3,81 @@ module FADD(
     input wire [31:0] B,
     output wire [31:0] Result
 );
-    wire [8:0] exponentA, exponentB, shift, shiftNegative;
-    wire [7:0] exponentC, exponentResult;
-    wire [23:0] fractionA, fractionB, mantissaA, mantissaB;
-    wire [24:0] mantissaC;
-    wire [22:0] mantissaResult;
+    // 32-bit internal wires
+    wire [8:0] exponentA32, exponentB32, shift32, shiftNegative32;
+    wire [7:0] exponentC32, exponentResult32;
+    wire [23:0] fractionA32, fractionB32, mantissaA32, mantissaB32;
+    wire [24:0] mantissaC32;
+    wire [22:0] mantissaResult32;    
+    wire [31:0] Result32;
     
-    // Step 1
-    assign exponentA[8] = 1'b0;
-    assign exponentA[7:0] = A[30:23];
-    assign exponentB[8] = 1'b0;
-    assign exponentB[7:0] = B[30:23];
+    // 16-bit internal wires
+    wire [5:0] exponentA16, exponentB16, shift16, shiftNegative16;
+    wire [4:0] exponentC16, exponentResult16;
+    wire [10:0] fractionA16, fractionB16, mantissaA16, mantissaB16;
+    wire [11:0] mantissaC16;
+    wire [9:0] mantissaResult16;
+    wire [15:0] RawResult16;
     
-    // Step 2
-    assign fractionA[23] = 1'b1;
-    assign fractionA[22:0] = A[22:0];
-    assign fractionB[23] = 1'b1;
-    assign fractionB[22:0] = B[22:0];
+    // is it 16 bit?
+    wire is16bit = (A[31:16] == 16'b0) && (B[31:16] == 16'b0);
     
-    // Step 3
-    assign shift = exponentA + ~exponentB + 1;
-    assign exponentC = (shift[8]) ? exponentB[7:0]: exponentA[7:0];
-    // Step 4
-    assign shiftNegative = ~shift + 1;
-    assign mantissaA = (shift[8]) ? fractionB: fractionA;
-    assign mantissaB = (shift[8]) ? fractionA >> shiftNegative: fractionB >> shift;
-    // Step 5
-    assign mantissaC = mantissaA + mantissaB;    
-    // Step 6
-    assign exponentResult = exponentC + mantissaC[24];
-    assign mantissaResult = (mantissaC[24]) ? mantissaC[23:1] : mantissaC[22:0];
-    // Step 7
-    assign Result = {A[31], exponentResult, mantissaResult};
+    // Step 1 32-bit
+    assign exponentA32[8] = 1'b0;
+    assign exponentA32[7:0] = A[30:23];
+    assign exponentB32[8] = 1'b0;
+    assign exponentB32[7:0] = B[30:23];
+    // Step 2 32-bit
+    assign fractionA32[23] = 1'b1;
+    assign fractionA32[22:0] = A[22:0];
+    assign fractionB32[23] = 1'b1;
+    assign fractionB32[22:0] = B[22:0];
+    // Step 3 32-bit
+    assign shift32 = exponentA32 + ~exponentB32 + 1;
+    assign exponentC32 = (shift32[8]) ? exponentB32[7:0]: exponentA32[7:0];
+    // Step 4 32-bit
+    assign shiftNegative32 = ~shift32 + 1;
+    assign mantissaA32 = (shift32[8]) ? fractionB32 : fractionA32;
+    assign mantissaB32 = (shift32[8]) ? fractionA32 >> shiftNegative32 : fractionB32 >> shift32;
+    // Step 5 32-bit
+    assign mantissaC32 = mantissaA32 + mantissaB32;  
+    // Step 6 32-bit
+    assign exponentResult32 = exponentC32 + mantissaC32[24];
+    assign mantissaResult32 = (mantissaC32[24]) ? mantissaC32[23:1] : mantissaC32[22:0];
+    // Step 7 32-bit
+    assign Result32 = {A[31], exponentResult32, mantissaResult32};
+    
+    
+    
+    // Step 1 16-bit
+    assign exponentA16[5] = 1'b0;
+    assign exponentA16[4:0] = A[14:10];
+    assign exponentB16[5] = 1'b0;
+    assign exponentB16[4:0] = B[14:10];        
+    // Step 2 16-bit
+    assign fractionA16[10] = 1'b1;
+    assign fractionA16[9:0] = A[9:0];
+    assign fractionB16[10] = 1'b1;
+    assign fractionB16[9:0] = B[9:0];
+    // Step 3 16-bit   
+    assign shift16 = exponentA16 + ~exponentB16 + 1;
+    assign exponentC16 = (shift16[5]) ? exponentB16[4:0]: exponentA16[4:0];
+    // Step 4 16-bit
+    assign shiftNegative16 = ~shift16 + 1;
+    assign mantissaA16 = (shift16[5]) ? fractionB16 : fractionA16;
+    assign mantissaB16 = (shift16[5]) ? fractionA16 >> shiftNegative16 : fractionB16 >> shift16;
+    // Step 5 16-bit
+    assign mantissaC16 = mantissaA16 + mantissaB16;
+    // Step 6 16-bit
+    assign exponentResult16 = exponentC16 + mantissaC16[11];
+    assign mantissaResult16 = (mantissaC16[11]) ? mantissaC16[10:1] : mantissaC16[9:0];
+    // Step 7 16-bit
+    assign RawResult16 = {A[15], exponentResult16, mantissaResult16};
+    
+    //Extend the 16-bit result to 32-bits by filling the upper bits with zeros
+    wire [31:0] ExtendedResult16;
+    assign ExtendedResult16 = {16'b0 , RawResult16};
+    
+    assign Result = is16bit ? ExtendedResult16 : Result32;
 
 endmodule
--- Single Cycle/mulFP.v
+++ Single Cycle/mulFP.v
@@ -3,30 +3,60 @@ module FMUL(
     input wire [31:0] FPB,
     output wire [31:0] FPResult
 );
-    wire [23:0] man_a, man_b;
-    wire [22:0] man_res;
-    wire [7:0] exp_a, exp_b;
-    wire [8:0] exp_res, exp_res2;
-    wire sig_a, sig_b, sig_res;
-    wire [47:0] product;
+    // 32-bit internal wires
+    wire [23:0] man_a32, man_b32;
+    wire [22:0] man_res32;
+    wire [7:0] exp_a32, exp_b32;
+    wire [8:0] exp_res32, exp_res232;
+    wire sig_a32, sig_b32, sig_res32;
+    wire [47:0] product32;
 
-    assign sig_a = FPA[31];
-    assign sig_b = FPB[31];
+    // 16-bit internal wires
+    wire [10:0] man_a16, man_b16;
+    wire [9:0] man_res16;
+    wire [4:0] exp_a16, exp_b16;
+    wire [5:0] exp_res16, exp_res216;
+    wire sig_a16, sig_b16, sig_res16;
+    wire [21:0] product16;
 
-    assign exp_a = FPA[30:23];
-    assign exp_b = FPB[30:23];
+    // Determine if inputs are 16-bit
+    wire is16bit = (FPA[31:16] == 16'b0) && (FPB[31:16] == 16'b0);
 
-    assign man_a[23] = 1'b1;
-    assign man_a[22:0] = FPA[22:0];
-    assign man_b[23] = 1'b1;
-    assign man_b[22:0] = FPB[22:0];
+    // 32-bit operations
+    assign sig_a32 = FPA[31];
+    assign sig_b32 = FPB[31];
+    assign exp_a32 = FPA[30:23];
+    assign exp_b32 = FPB[30:23];
+    assign man_a32[23] = 1'b1;
+    assign man_a32[22:0] = FPA[22:0];
+    assign man_b32[23] = 1'b1;
+    assign man_b32[22:0] = FPB[22:0];
+    assign sig_res32 = sig_a32 ^ sig_b32;
+    assign exp_res32 = exp_a32 + exp_b32 + 8'b10000001;
+    assign product32 = man_a32 * man_b32;
+    assign exp_res232 = exp_res32 + product32[47];
+    assign man_res32 = (product32[47]) ? product32[46:24] : product32[45:23];
+    wire [31:0] Result32 = {sig_res32, exp_res232[7:0], man_res32};
 
-    assign sig_res = sig_a ^ sig_b;
-    assign exp_res = exp_a + exp_b + 8'b10000001;
-    assign product = man_a * man_b;
+    // 16-bit operations
+    assign sig_a16 = FPA[15];
+    assign sig_b16 = FPB[15];
+    assign exp_a16 = FPA[14:10];
+    assign exp_b16 = FPB[14:10];
+    assign man_a16[10] = 1'b1;
+    assign man_a16[9:0] = FPA[9:0];
+    assign man_b16[10] = 1'b1;
+    assign man_b16[9:0] = FPB[9:0];
+    assign sig_res16 = sig_a16 ^ sig_b16;
+    assign exp_res16 = exp_a16 + exp_b16 + 6'b011111;
+    assign product16 = man_a16 * man_b16;
+    assign exp_res216 = exp_res16 + product16[21];
+    assign man_res16 = (product16[21]) ? product16[20:10] : product16[19:9];
+    wire [15:0] RawResult16 = {sig_res16, exp_res216[4:0], man_res16};
 
-    assign exp_res2 = exp_res + product[47];
-    assign man_res = (product[47]) ? product[46:24] : product[45:23];
+    // Extend 16-bit result to 32-bit by filling the upper bits with zeros
+    wire [31:0] ExtendedResult16 = {16'b0, RawResult16};
 
-    assign FPResult = { sig_res, exp_res2[7:0], man_res};
+    // Select the appropriate result based on input bit width
+    assign FPResult = is16bit ? ExtendedResult16 : Result32;
 endmodule
